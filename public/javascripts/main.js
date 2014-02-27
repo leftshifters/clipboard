66522390dfba865f0573b4c3120b1532dcf2cab1
Debjeet Biswas <debjeet@vxtindia.com>
2014-02-27 22:26:45 +0530

    WIP

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,6 +5,7 @@
   var form = document.querySelector('.upload-form');
   var btnSubmit = document.querySelector('.btn-submit');
   var namefield = document.querySelector('#name');
+  var items = document.querySelectorAll('.item');
 
   var shouldSubmit = false;
 
@@ -24,5 +25,50 @@
     btnSubmit.removeAttribute('disabled');
     namefield.focus();
   });
+
+
+  bind(items, delegate);
+
+  function bind(els, listener, eventName, capture) {
+    eventName = eventName || 'click';
+    capture = capture || false;
+    for (var i = 0, len = els.length; i < len; ++i) {
+      els[i].addEventListener(eventName, listener, capture);
+    }
+  }
+
+  function delegate(e) {
+    var overlay = this.querySelector('.item-overlay');
+    var el = e.target;
+    console.log(overlay);
+    toggleClass(overlay, 'hide');
+    // if (el.classList.contains('js-cog')) {
+    //   oncogclick(el);
+    // } else if (e.target === '') {
+
+    // }
+  }
+
+  function oncogclick(el) {
+    console.log(el.parentNode);
+    document.querySelector()
+  }
+
+  function toggleClass(el, className) {
+    className = className || '';
+    if (el.classList) {
+      el.classList.toggle(className);
+    } else {
+      var classes = el.className.split(' ');
+      var existingIndex = classes.indexOf(className);
+
+      if (existingIndex >= 0)
+        classes.splice(existingIndex, 1);
+      else
+        classes.push(className);
+
+      el.className = classes.join(' ');
+    }
+  }
 
 })(window);
